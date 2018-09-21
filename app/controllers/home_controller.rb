a80a76107505257c4badae7ee12b25fb0dc45376
nori0610 <nishiie@ivy.ocn.ne.jp>
2018-09-21 23:32:23 +0900

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  before_action :forbid_login_user, {only: [:top]}
+
   def top
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def about
   end
 end
