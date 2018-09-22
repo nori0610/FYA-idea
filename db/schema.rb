a2fd65b181b4a5d8e03b21c1a600c820d7b0d92c
nori0610 <nishiie@ivy.ocn.ne.jp>
2018-09-23 00:17:22 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_143932) do
+ActiveRecord::Schema.define(version: 2018_08_14_001019) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +44,8 @@ ActiveRecord::Schema.define(version: 2018_06_18_143932) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,6 +53,8 @@ ActiveRecord::Schema.define(version: 2018_06_18_143932) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
+    t.string "password_digest"
   end
 
 end
