f97bf905122f9a893acc53ef069cd8c8605d0300
nori0610 <nishiie@ivy.ocn.ne.jp>
2018-09-28 23:57:56 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_001019) do
+ActiveRecord::Schema.define(version: 2018_08_15_000437) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2018_08_14_001019) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "picture_id"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -46,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_001019) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "title"
+    t.integer "category"
   end
 
   create_table "users", force: :cascade do |t|
