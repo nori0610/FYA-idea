76e6fa247114cb9af18d882c2221eb58d0b68377
yuto_bknb <yuto.suzuki.226@gmail.com>
2018-10-14 17:18:48 +0900

    カテゴリー選別　トップページに実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,12 @@ class HomeController < ApplicationController
     elsif
       if params[:options][:likes]
         logger.debug 'likes'
-        #likes = Post.likes
-        @posts = Post.all
+        @posts = Post.joins(:likes).group("likes.post_id").order('count(likes.post_id) desc')
       elsif params[:options][:allViews]
         logger.debug 'ok!'
         @posts = Post.all.order(created_at: :desc)
+      elsif params[:options][:category] != nil
+        @posts = Post.where(category: params[:options][:category].to_i)
       end
     end
 
