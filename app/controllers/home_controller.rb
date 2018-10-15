2f690c7b7022a957c6eaa0b145b485e0bd11f502
nori0610 <nishiie@ivy.ocn.ne.jp>
2018-10-15 23:49:05 +0900

    ソート実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,20 +2,14 @@ class HomeController < ApplicationController
 
   def top
     if params[:options].nil?
-      logger.debug 'こっち'
       @posts = Post.limit(6).order(created_at: :desc) #6件取得
-    elsif
-      if params[:options][:likes]
-        logger.debug 'likes'
-        @posts = Post.joins(:likes).group("likes.post_id").order('count(likes.post_id) desc')
-      elsif params[:options][:allViews]
-        logger.debug 'ok!'
-        @posts = Post.all.order(created_at: :desc)
-      elsif params[:options][:category] != nil
-        @posts = Post.where(category: params[:options][:category].to_i)
-      end
+      @likes = Post.joins(:likes).group("likes.post_id").limit(6).order('count(likes.post_id) desc')
+    elsif params[:options][:allViews]
+      @likes = Post.joins(:likes).group("likes.post_id").order('count(likes.post_id) desc')
+      @posts = Post.all.order(created_at: :desc)
+    elsif params[:options][:category] != nil
+      @posts = Post.where(category: params[:options][:category].to_i)
     end
-
   end
 
   def about
