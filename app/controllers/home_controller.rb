e76f6f8ee7d2deaeebab8f1b24731d13644b5bec
nori0610 <nishiie@ivy.ocn.ne.jp>
2018-10-17 20:49:09 +0900

    herokuエラーを修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
   def top
     if params[:options].nil?
       @posts = Post.limit(6).order(created_at: :desc) #6件取得
-      @likes = Post.joins(:likes).group("likes.post_id").limit(6).order('count(likes.post_id) desc')
+      @likes = Post.joins(:likes).group("posts.id").limit(6).order('count(posts.id) desc')
     elsif params[:options][:allViews]
-      @likes = Post.joins(:likes).group("likes.post_id").order('count(likes.post_id) desc')
+      @likes = Post.joins(:likes).group("posts.id").order('count(posts.id) desc')
       @posts = Post.all.order(created_at: :desc)
     elsif params[:options][:category] != nil
       @posts = Post.where(category: params[:options][:category].to_i)
