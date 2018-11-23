31efdb71d02c45c1a888a600a49a9bf3095131aa
nori0610 <nishiie@ivy.ocn.ne.jp>
2018-11-23 22:06:34 +0900

    to top & footer js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,65 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// page to top
+$(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+// footer
+$(function() {
+    var winWidth = $('body').outerWidth(true);
+    var footer = $('#footer');
+    var slide = $('#slide');
+    //画面下位置を取得
+    var bottomPos = $(document).height() - $(window).height() -500;
+    var showFlug = false;
+
+    // ウィンドウより小さかったら開く
+    panelOpen();
+    //slideを画面右外へ配置
+    $('#slide').css('margin-left', winWidth+'px');
+    $(window).scroll(function () {
+        panelOpen();
+    });
+    //閉じるボタン
+    $('#close').click(function(){
+        footer.hide();
+    });
+    //ウィンドウリサイズしたらwidth変更
+    $(window).resize(function(){
+        winWidth = $('body').outerWidth(true);
+        bottomPos = $(document).height() - $(window).height() - 500;
+    });
+
+    function panelOpen() {
+        if ($(this).scrollTop() >= bottomPos) {
+            if (showFlug == false) {
+                showFlug = true;
+                slide.stop().animate({'marginLeft' : '0px'}, 200);
+            }
+        } else {
+            if (showFlug) {
+                showFlug = false;
+                slide.stop().animate({'marginLeft' : winWidth+'px'}, 200);
+            }
+        }
+    }
+});
