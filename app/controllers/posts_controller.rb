38013d91af7593c034f479bb8698f382cc32fb81
nori0610 <nishiie@ivy.ocn.ne.jp>
2018-12-31 02:27:52 +0900

    search category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
   before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @q     = Post.ransack(params[:q])
+    @posts = @q.result.order(created_at: :desc)
   end
 
   def show
