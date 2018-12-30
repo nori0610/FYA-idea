cbbdb318c545a5366057f7565c1fc09de461c870
yukimura1227 <takamura1227@gmail.com>
2018-12-30 18:21:17 +0900

    feat: sort by updated_at function

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  belongs_to :post, touch: true # NOTE: touch: trueとしているのは、postsを更新順でソートした際に、comment投稿も考慮するため
   validates :content, presence: true
   has_one_attached :image
 end
