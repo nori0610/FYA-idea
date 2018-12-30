120f03acbff6a25b00dfef9a32fd85285aef6c6b
yukimura1227 <takamura1227@gmail.com>
2018-12-30 23:41:16 +0900

    feat: show random 6 posts on top page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,17 @@ class HomeController < ApplicationController
       @posts = Post.limit(6).order(created_at: :desc) #6件取得
       @likes = Post.joins(:likes).group("posts.id").limit(6).order('count(posts.id) desc')
       @posts_updated_order = Post.limit(6).order(Post::SELECT_MAP_FOR_SORT[:'更新順'])
+      @posts_random = Post.all.sample(6)
     elsif params[:options][:allViews]
       @likes = Post.joins(:likes).group("posts.id").order('count(posts.id) desc')
       @posts = Post.all.order(created_at: :desc)
       @posts_updated_order = Post.order(Post::SELECT_MAP_FOR_SORT[:'更新順'])
+      @posts_random = Post.all.sample(6)
     elsif params[:options][:category] != nil
       @posts = Post.where(category: params[:options][:category].to_i)
       @likes = []
       @posts_updated_order = []
+      @posts_random = Post.where(category: params[:options][:category].to_i).sample(6)
     end
   end
 
