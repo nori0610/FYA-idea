8612e7ba9f5402ab1e3b52f7fb41ab6cdc7600bb
Yuta Takahashi <m1205123ing@gmail.com>
2019-04-29 13:42:54 +0900

    ライク機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   def posts
     return Post.where(user_id: self.id)
   end
+
+  # 対象postにライクをしたかどうか
+  def have_like?(post_id)
+    if Like.find_by(user_id: id, post_id: post_id).present?
+      return true
+    end
+    false
+  end
 end
